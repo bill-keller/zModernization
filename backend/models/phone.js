3e56fe4f5e369bc2cbbdc0846d3b0776e5c16729
Bill Keller <bkeller99@gmail.com>
2022-03-15 16:11:44 -0500

    updated phone.js model to include new mongo fields

--- backend/models/phone.js
+++ backend/models/phone.js
@@ -4,7 +4,12 @@ const phoneSchema = mongoose.Schema({
   itemID: { type: String, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
-  imagePath: { type: String, required: true }
+  imagePath: { type: String, required: true },
+  RAM: { type: String, required: true},
+  internalStorage: { type: String, required: true},
+  screenResolution: { type: String, required: true},
+  screenSize: { type: String, required: true},
+
 });
 
 module.exports = mongoose.model("Phone", phoneSchema);
--- backend/routes/phones.js
+++ backend/routes/phones.js
@@ -1,41 +1,15 @@
 const express = require("express");
-const multer = require("multer");
 const axios = require("axios");
 
 const Phone = require("../models/phone");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
 router.get("/:id", (req, res, next) => {
 
   function getCics() {
     return axios.get("http://192.168.48.127:19890/catalog/items/"+req.params.id);
-  }  
+  }
   function getDb2() {
     return axios.get("http://192.168.48.127:19890/catalog_device_dimensions/devices/"+req.params.id);
   }
@@ -65,11 +39,11 @@ router.get("", (req, res, next) => {
 
   function getCics() {
     return axios.get('http://192.168.48.127:19890/catalog/items?startItemID=0010');
-  }  
+  }
   function getMongo(){
     return Phone.find().lean().exec();
   }
-  
+
   Promise.all([getCics(), getMongo()])
     .then(function (results) {
       const cicsItems = results[0].data.cics_cat_resp.inquire_request.cat_item;
@@ -86,69 +60,5 @@ router.get("", (req, res, next) => {
     })
 });
 
-
-router.post(
-  "",
-  multer({
-    storage: storage
-  }).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      itemID: 10
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        post: {
-          ...createdPost,
-          id: createdPost._id
-        }
-      });
-    });
-  }
-);
-
-router.put(
-  "/:id",
-  multer({
-    storage: storage
-  }).single("image"),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath
-    });
-    console.log(post);
-    Post.updateOne({
-      _id: req.params.id
-    }, post).then(result => {
-      res.status(200).json({
-        message: "Update successful!"
-      });
-    });
-  }
-);
-
-router.delete("/:id", (req, res, next) => {
-  Post.deleteOne({
-    _id: req.params.id
-  }).then(result => {
-    console.log(result);
-    res.status(200).json({
-      message: "Post deleted!"
-    });
-  });
-});
 
 module.exports = router;
