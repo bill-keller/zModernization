ebf64aaf0582219337eb54ad3f71f22e1e586bbc
Bill Keller <bkeller99@gmail.com>
2022-04-29 15:54:52 -0500

    add comments to backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 
-require('dotenv').config();
+require('dotenv').config();    // read env vars from .env file
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
